e99086ffa6ffec243a17fc4efe9d041dfd488b65
Manuel Frezzato <manuel.frezzato@gmail.com>
2023-12-13 19:52:32 +0100

    feat | Esercizio completato

--- src/components/JokeCard.jsx
+++ src/components/JokeCard.jsx
@@ -4,12 +4,13 @@ import { useState } from "react";
 export default () => {
   const [joke, setJoke] = useState();
   const [click, setClick] = useState(false);
+  const [reload, setReload] = useState(true);
   useEffect(() => {
     fetch("https://v2.jokeapi.dev/joke/Programming?type=twopart")
       .then((response) => response.json())
       .then((obj) => setJoke(obj))
       .catch((error) => console.error(error));
-  }, []);
+  }, [reload]);
   return (
     <>
       <div>
@@ -18,10 +19,22 @@ export default () => {
         ) : (
           <div>
             <h3>{joke.setup}</h3>
-            <button onClick={() => setClick(!click)}>
-              {click === true ? "Reload" : "Answer"}
-            </button>
-            <p>{click === true ? joke.delivery : ""}</p>
+            {click === true ? (
+              <div>
+                <p>{joke.delivery}</p>
+                <button
+                  onClick={() => {
+                    return setClick(!click), setReload(!reload);
+                  }}
+                >
+                  Reload
+                </button>
+              </div>
+            ) : (
+              <div>
+                <button onClick={() => setClick(!click)}>Answer</button>
+              </div>
+            )}
           </div>
         )}
       </div>
